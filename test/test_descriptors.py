84f1b3260ecc3cd5654575dfefa6fb42abc253ce
GardevoirX <xqj.2001@outlook.com>
2024-04-29 20:03:30 +0200

    Tests update

--- test/test_descriptors.py
+++ test/test_descriptors.py
@@ -1,10 +1,12 @@
+import pytest
 from src.descriptors import AVAILABLE_DESCRIPTORS
 
 TEST_SMILES = 'CC(=O)Nc1ccc(cc1)O'
 
-def test_descriptors():
-    for descriptor in AVAILABLE_DESCRIPTORS:
-        des = descriptor()
-        result = des(TEST_SMILES)
-        assert isinstance(result, list)
-        assert len(result) == 1
+
+@pytest.mark.parametrize("descriptor", AVAILABLE_DESCRIPTORS)
+def test_descriptors(descriptor):
+    des = descriptor()
+    result = des(TEST_SMILES)
+    assert isinstance(result, list)
+    assert len(result) == 1
--- test/test_models.py
+++ test/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.models import AVAILABLE_MODELS
 
 TEST_DATA = [
@@ -16,10 +18,10 @@ TEST_LABELS = [
     1
 ]
 
-def test_models():
-    for model in AVAILABLE_MODELS:
-        model = model()
-        print(model)
-        model.fit(TEST_DATA, TEST_LABELS)
-        result = model.predict(TEST_DATA)
-        assert len(result) == 5
+
+@pytest.mark.parametrize("test_model", AVAILABLE_MODELS)
+def test_models(test_model):
+    model = test_model()
+    model.fit(TEST_DATA, TEST_LABELS)
+    result = model.predict(TEST_DATA)
+    assert len(result) == 5
