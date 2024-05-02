ef9679fbc3e6ee75f870017ade7d56035bb4dff5
Yunjie Xu <100222993+Syrizein@users.noreply.github.com>
2024-05-02 22:36:19 +0200

    Adding descriptor functions

--- src/descriptors/descriptors.py
+++ src/descriptors/descriptors.py
@@ -1,5 +1,6 @@
 from rdkit import Chem
-from rdkit.Chem.Descriptors import ExactMolWt
+from rdkit.Chem.Descriptors import ExactMolWt, MolLogP, TPSA, NumHAcceptors, NumHDonors, NumRotatableBonds, NumAromaticRings
+from rdkit.Chem.rdFreeSASA import CalcSASA, classifyAtoms
 
 from ._abc import DescriptorsABC
 
@@ -7,3 +8,46 @@ class MolWt(DescriptorsABC):
     def __call__(self, SMILES):
         mol = Chem.MolFromSmiles(SMILES)
         return [ExactMolWt(mol)]
+
+""" logP, partition coefficient (oil/ water) """
+
+class logP(DescriptorsABC):
+    def __call__(self, SMILES):
+        mol = Chem.MolFromSmiles(SMILES)
+        return [MolLogP(mol)]
+    
+""" topological polar surface area (TPSA) """
+
+class TPSA(DescriptorsABC):
+    def __call__(self, SMILES):
+        mol = Chem.MolFromSmiles(SMILES)
+        return [TPSA(mol)]
+
+""" numbers related to H-Bonds (like donor, acceptor) """
+
+class HBonds(DescriptorsABC):
+    def __call__(self, SMILES):
+        mol = Chem.MolFromSmiles(SMILES)
+        return [NumHAcceptors(mol), NumHDonors(mol)]
+
+""" solvent accessible surface area (SASA) """
+
+class SASA(DescriptorsABC):
+    def __call__(self, SMILES):
+        mol = Chem.MolFromSmiles(SMILES)
+        radius = classifyAtoms(mol)
+        return [CalcSASA(mol, radius)]
+
+""" number of rotatable bonds """
+
+class RotBond(DescriptorsABC):
+    def __call__(self, SMILES):
+        mol = Chem.MolFromSmiles(SMILES)
+        return [NumRotatableBonds(mol)]
+
+""" number of aromatic rings. """
+
+class AromaRing(DescriptorsABC):
+    def __call__(self, SMILES):
+        mol = Chem.MolFromSmiles(SMILES)
+        return [NumAromaticRings(mol)]
