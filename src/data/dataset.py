fff72ee3bc7939eacc975aa95e6cb0cbdf4ce62e
GardevoirX <xqj.2001@outlook.com>
2024-05-25 23:06:01 +0200

    Add standardization

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,28 +1,45 @@
 import pandas as pd
+import torch
 
-from torch import Tensor
+from rich.progress import track
+from torch import Tensor, any, isnan
 from torch.utils.data import Dataset
 
 
 class CNSDataset(Dataset):
-    def __init__(self, data_file: str, transform=None, target_transform=None):
+    def __init__(
+        self,
+        data_file: str,
+        transform=None,
+        target_transform=None,
+        device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+    ):
         self.raw_data = pd.read_csv(data_file)
         self.transform = transform
         self.target_transform = target_transform
+        self.device = device
+        self._do_transform()
 
     def __len__(self):
         return len(self.raw_data)
 
     def __getitem__(self, idx):
-        item = self.raw_data.iloc[idx]
-        properties = item["SMILES"]
-        label = item["TARGET"]
-        if self.transform is not None:
-            properties = Tensor(self.transform(properties))
-        if self.target_transform is not None:
-            label = self.target_transform(label)
 
-        return properties, label
+        return self._processed_data[idx], self._processed_label[idx]
+
+    def _do_transform(self):
+        if self.transform is None:
+            self._processed_data = self.raw_data["SMILES"]
+        else:
+            self._processed_data = Tensor(
+                [self.transform(smiles) for smiles in track(self.raw_data["SMILES"])]
+            ).to(self.device)
+        if self.target_transform is None:
+            self._processed_label = self.raw_data["TARGET"]
+        else:
+            self._processed_label = Tensor([
+                self.target_transform(label) for label in self.raw_data["TARGET"]
+            ]).to(self.device)
 
     @property
     def SMILES(self):
@@ -31,3 +48,9 @@ class CNSDataset(Dataset):
     @property
     def labels(self):
         return self.raw_data["TARGET"]
+
+    def standardize(self, means, stds):
+        self._processed_data = (self._processed_data - means) / stds
+        if any(mask := any(isnan(self._processed_data), axis=0)):
+            print("NaN in descriptors, remove it")
+        self._processed_data = self._processed_data[:, ~mask]
